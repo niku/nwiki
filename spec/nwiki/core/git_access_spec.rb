accaf3abe8283d9538e495e9e9729bc1840c0190
niku <niku@niku.name>
2012-06-17 23:07:27 +0900

    add spec for Core::GitAccess#tree

--- spec/nwiki/core/git_access_spec.rb
+++ spec/nwiki/core/git_access_spec.rb
@@ -5,8 +5,12 @@ module Nwiki
     describe GitAccess do
       let(:path) { 'spec/examples/sample.git' }
 
-      describe '#initialize' do
-        it { expect { described_class.new(path) }.to_not raise_error }
+      subject { described_class.new(path) }
+
+      describe '#tree' do
+        let(:ref) { '63c0856958172223da3309e653f837a3485be4ae' }
+
+        it { subject.tree(ref).should have_at_least(1).blob_entries }
       end
     end
   end
