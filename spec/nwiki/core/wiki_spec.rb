9947edf5bc56a47c172e9cb948460db5e1778005
niku <niku@niku.name>
2012-08-09 20:48:58 +0900

    fix spec description

--- spec/nwiki/core/wiki_spec.rb
+++ spec/nwiki/core/wiki_spec.rb
@@ -7,7 +7,7 @@ module Nwiki
       let(:path) { 'spec/examples/sample.git' }
       subject { described_class.new(path) }
 
-      describe '#page' do
+      describe '#find' do
         it { subject.find('/foo').should eq Page.new("Foo", "* Foo\n** Bar\n[[icon.png]]\n", Wiki.parser) }
         it { subject.find('/not_exist_page').should be_nil }
         it { subject.find('/1/2/a').should_not be_nil }
