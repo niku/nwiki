fefff9932c2aac67154ffb682fa3e3f5e63f2479
niku <niku@niku.name>
2013-02-06 07:38:16 +0900

    respond valid channel id

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -38,6 +38,7 @@ module Nwiki
           it { subject.subtitle.content.should eq 'Example Site' }
           it { subject.author.name.content.should eq 'Bob' }
           it { subject.date.should eq Time.parse('2014-02-06') }
+          it { subject.id.content.should eq '1' }
         end
 
       end
