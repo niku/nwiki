6eac22267279b72794344815e2b2dfb4cc887147
niku <niku@niku.name>
2013-07-10 06:50:49 +0900

    extract class to feed.rb

--- lib/nwiki/frontend/app.rb
+++ lib/nwiki/frontend/app.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 require 'erb'
-require 'rss'
 require_relative 'app/top'
+require_relative 'app/feed'
 
 module Nwiki
   module Frontend
@@ -22,54 +22,6 @@ module Nwiki
 
       def call env
         @builder.call env
-      end
-    end
-
-    class Feed
-      attr_reader :articles_path
-
-      def initialize git_repo_path, opts = {}
-        @wiki = Nwiki::Core::Wiki.new git_repo_path
-        raise unless @wiki.exist?
-        @articles_path = opts[:articles_path] || ''
-      end
-
-      def call env
-        [
-          200,
-          { 'Content-Type' => "application/atom+xml; charset=#{Nwiki::Core::Wiki.repo_filename_encoding}" },
-          [
-            RSS::Maker.make('atom') { |maker|
-              maker.channel.title = @wiki.title
-              maker.channel.description = @wiki.subtitle
-              maker.channel.link = Rack::Request.new(env).url
-
-              maker.channel.author = @wiki.author
-              maker.channel.date = Time.parse('2014-02-06')
-              maker.channel.id = Rack::Request.new(env).url
-
-              maker.items.do_sort = true
-              maker.items.max_size = 50
-
-              log = @wiki.access.repo.log
-              log.each do |commit|
-                date = commit.date
-                commit.show.each do |diff|
-                  next unless diff.new_file
-
-                  path = Nwiki::Core::Wiki.canonicalize_path(diff.b_path)
-                  path.gsub!(/\.org$/, '')
-
-                  maker.items.new_item do |item|
-                    item.link = Rack::Request.new(env).url.gsub(Regexp.new(Rack::Request.new(env).fullpath), "#{articles_path}/#{path}")
-                    item.title = File.basename(path)
-                    item.date = date
-                  end
-                end
-              end
-            }.to_s
-          ]
-        ]
       end
     end
 
