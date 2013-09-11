b673c79f59a820c2934a880e7b2b857d994d41b3
niku <niku@niku.name>
2013-09-12 00:54:14 +0900

    implement NewGitAccess#title

--- lib/nwiki/core/git_access.rb
+++ lib/nwiki/core/git_access.rb
@@ -1,8 +1,24 @@
 require 'gollum-lib'
+require 'rugged'
 
 module Nwiki
   module Core
     # copy from gollum
     GitAccess = ::Gollum::GitAccess
+    class NewGitAccess
+      def initialize repo_path
+        @repo = Rugged::Repository.new(::File.expand_path(repo_path))
+      end
+
+      def config
+        Rugged::Branch.lookup(@repo, 'config')
+      end
+
+      def title
+        title_entry = config.tip.tree.get_entry('title')
+        title_blob = @repo.lookup(title_entry[:oid])
+        title_blob.text.chomp
+      end
+    end
   end
 end
--- spec/nwiki/core/git_access_spec.rb
+++ spec/nwiki/core/git_access_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 module Nwiki
@@ -11,6 +12,16 @@ module Nwiki
         let(:ref) { '63c0856958172223da3309e653f837a3485be4ae' }
 
         it { subject.tree(ref).should have_at_least(1).blob_entries }
+      end
+    end
+
+    describe NewGitAccess do
+      let(:path) { 'spec/examples/sample.git' }
+
+      subject { described_class.new(path) }
+
+      describe '#title' do
+        it { expect(subject.title).to eq 'ヽ（´・肉・｀）ノログ' }
       end
     end
   end
