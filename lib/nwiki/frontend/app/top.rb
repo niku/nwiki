a372770c4564e95f1c4d9e35fa28e3861a48f4d2
niku <niku@niku.name>
2013-12-10 07:48:26 +0900

    update twitter bootstrap

--- lib/nwiki/frontend/app/top.rb
+++ lib/nwiki/frontend/app/top.rb
@@ -25,8 +25,8 @@ module Nwiki
   <title><%= @wiki.title %></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="alternate" type="application/atom+xml" title="ATOM Feed" href="/articles.xml">
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
 </head>
 <body>
   <a href="https://github.com/niku/nikulog">
@@ -46,7 +46,7 @@ module Nwiki
     </div>
   </div>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 </body>
 </html>
 EOS
