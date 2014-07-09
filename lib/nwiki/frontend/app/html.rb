f2249af9f862411c2eb4cddbe486c02653215491
niku <niku@niku.name>
2014-07-09 22:13:45 +0900

    Upgrade jquery

--- lib/nwiki/frontend/app/html.rb
+++ lib/nwiki/frontend/app/html.rb
@@ -49,7 +49,7 @@ module Nwiki
       </div>
     </div>
   </div>
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
 </html>
