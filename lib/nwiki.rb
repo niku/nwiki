317d1dce9c67e618da5cccf69361a561a6e6988c
niku <niku@niku.name>
2017-07-15 18:56:44 +0900

    Move to Nwiki.add_metadata

--- lib/nwiki.rb
+++ lib/nwiki.rb
@@ -2,6 +2,41 @@ require "nwiki/version"
 
 module Nwiki
   class << self
+    def template
+      ERB.new(<<__EOD__, nil, "-")
+<!DOCTYPE html>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+<meta charset="UTF-8">
+<meta name="viewport" content="width-device-width,initial-scale=1">
+<title><%= title %> - <%= ENV['NWIKI_SITE_NAME'] %></title>
+<meta content="<%= title %> - <%= ENV['NWIKI_SITE_NAME'] %>" name="title">
+<% if description -%>
+<meta content="<%= description %>" name="description">
+<% end -%>
+
+<meta property="og:type" content="article"/>
+<meta property="og:title" content="<%= title %> - <%= ENV['NWIKI_SITE_NAME'] %>"/>
+<% if description -%>
+<meta property="og:description" content="<%= description %>" />
+<% end -%>
+<% if image_url -%>
+<meta property="og:image" content="<%= image_url %>" />
+<% end -%>
+<meta property="og:url" content="<%= url %>" />
+<meta property="og:site_name" content="<%= ENV['NWIKI_SITE_NAME'] %>"/>
+</head>
+<body>
+<h1><a href="<%= ENV['NWIKI_ENDPOINT'] %>"><%= ENV['NWIKI_SITE_NAME'] %></a></h1>
+<h2><%= ENV['NWIKI_TAGLINE'] %></h2>
+<article>
+<%= html_contents -%>
+</article>
+</body>
+</html>
+__EOD__
+    end
+
     def get_head(temporary_path, repo_url)
       require "rugged"
       if File.exist?(temporary_path)
@@ -20,6 +55,35 @@ module Nwiki
         doc = File.read(path)
         html = Orgmode::Parser.new(doc, allow_include_files: true).to_html
         File.write(new_path, html)
+        File.delete(path)
+      end
+    end
+
+    def add_metadata(temporary_path, endpoint)
+      require "nokogiri"
+      FileList.new("#{temporary_path}/**/*.html.contents").each do |path|
+        new_path = File.join(File.dirname(path), File.basename(path, ".contents"))
+        # `tmp/foo/bar.html` ->
+        #     `foo/bar.html`
+        dir_path_without_root = File.dirname(path).slice(temporary_path.length + 1..-1)
+        url = if dir_path_without_root
+                endpoint + dir_path_without_root + "/" + File.basename(path, ".contents")
+              else
+                endpoint + File.basename(path, ".contents")
+              end
+        title = File.basename(path, ".html.contents")
+        html_contents = File.read(path)
+        parsed_document = Nokogiri::HTML(html_contents)
+        description = if desc = parsed_document.at_xpath("//p")
+                        desc.text
+                      end
+        image = if img = parsed_document.at_xpath("//img/@src")
+                  img.value
+                end
+        image_url = if image
+                      dir_path_without_root ? endpoint + dir_path_without_root + "/" + image : endpoint + image
+                    end
+        File.write(new_path, template.result(binding))
         File.delete(path)
       end
     end
--- rakelib/nwiki.rake
+++ rakelib/nwiki.rake
@@ -11,39 +11,6 @@ namespace :nwiki do
   temporary_path = ENV.fetch("NWIKI_TEMPORARY_PATH", "tmp")
   CLOBBER.include(temporary_path)
 
-  template = ERB.new(<<__EOD__, nil, "-")
-<!DOCTYPE html>
-<html lang="ja">
-<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
-<meta charset="UTF-8">
-<meta name="viewport" content="width-device-width,initial-scale=1">
-<title><%= title %> - <%= ENV['NWIKI_SITE_NAME'] %></title>
-<meta content="<%= title %> - <%= ENV['NWIKI_SITE_NAME'] %>" name="title">
-<% if description -%>
-<meta content="<%= description %>" name="description">
-<% end -%>
-
-<meta property="og:type" content="article"/>
-<meta property="og:title" content="<%= title %> - <%= ENV['NWIKI_SITE_NAME'] %>"/>
-<% if description -%>
-<meta property="og:description" content="<%= description %>" />
-<% end -%>
-<% if image_url -%>
-<meta property="og:image" content="<%= image_url %>" />
-<% end -%>
-<meta property="og:url" content="<%= url %>" />
-<meta property="og:site_name" content="<%= ENV['NWIKI_SITE_NAME'] %>"/>
-</head>
-<body>
-<h1><a href="<%= ENV['NWIKI_ENDPOINT'] %>"><%= ENV['NWIKI_SITE_NAME'] %></a></h1>
-<h2><%= ENV['NWIKI_TAGLINE'] %></h2>
-<article>
-<%= html_contents -%>
-</article>
-</body>
-</html>
-__EOD__
-
   desc "get head of a remote git repository"
   task :get_head do
     Nwiki.get_head(temporary_path, ENV.fetch("NWIKI_REPO"))
@@ -56,33 +23,7 @@ __EOD__
 
   desc "add metadata to converted html contents"
   task :add_metadata do
-    require "nokogiri"
-    endpoint = ENV["NWIKI_ENDPOINT"]
-    FileList.new("#{temporary_path}/**/*.html.contents").each do |path|
-      new_path = File.join(File.dirname(path), File.basename(path, ".contents"))
-      # `tmp/foo/bar.html` ->
-      #     `foo/bar.html`
-      dir_path_without_root = File.dirname(path).slice(temporary_path.length + 1..-1)
-      url = if dir_path_without_root
-              endpoint + dir_path_without_root + "/" + File.basename(path, ".contents")
-            else
-              endpoint + File.basename(path, ".contents")
-            end
-      title = File.basename(path, ".html.contents")
-      html_contents = File.read(path)
-      parsed_document = Nokogiri::HTML(html_contents)
-      description = if desc = parsed_document.at_xpath("//p")
-                      desc.text
-                    end
-      image = if img = parsed_document.at_xpath("//img/@src")
-                img.value
-              end
-      image_url = if image
-                    dir_path_without_root ? endpoint + dir_path_without_root + "/" + image : endpoint + image
-                  end
-      File.write(new_path, template.result(binding))
-      File.delete(path)
-    end
+    Nwiki.add_metadata(temporary_path, ENV["NWIKI_ENDPOINT"])
   end
 
   desc "generate index"
