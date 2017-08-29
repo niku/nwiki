407814b474b9e789c4748a10d235023240b6024c
niku <niku@niku.name>
2017-08-29 20:15:38 +0900

    Add hatebu button

--- lib/nwiki.rb
+++ lib/nwiki.rb
@@ -34,6 +34,7 @@ module Nwiki
 <h2><%= ENV['NWIKI_TAGLINE'] %></h2>
 <article>
 <%= html_contents -%>
+<a href="http://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="vertical-normal" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
 </article>
 </body>
 </html>
