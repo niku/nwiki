dbbaca08518ec3826460b6abb66854b83b6dcc3a
niku <niku@niku.name>
2017-08-29 20:37:55 +0900

    Get rid of dependency to minitest

--- test/nwiki_test.rb
+++ test/nwiki_test.rb
@@ -1,11 +1,1 @@
 require 'test_helper'
-
-class NwikiTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Nwiki::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert "something useful"
-  end
-end
